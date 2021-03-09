a0dfb178b0d5f6a84e44c467879863c2328564de
Dreeedy <dreeedyone@gmail.com>
2021-03-09 16:21:59 +0300

    death acts / Реализовал регистрацию

--- blocksMain/acts_death_main_create.php
+++ blocksMain/acts_death_main_create.php
@@ -35,82 +35,73 @@
 
         </div>
         <div class="col-md-8 col-lg-9">
-            <h4 class="mb-3">Создание акта о смерти</h4>
+            <h4 class="mb-3">Регистрация акта о смерти</h4>
             <form action="../controllers/actsDeathController.php" method="post" class="needs-validation">
 
                 <div class="row g-3 mb-2">
 
-                    <!--01 - Муж - Open -->
+                    <!--01 - Customer - Open -->
                     <div class="col-12">
-                        <label for="husband_surname" class="visually-hidden">Фамилия мужа</label>
-                        <input type="text" class="form-control" id="husband_surname" name="husband_surname"
-                               placeholder="Фамилия мужа"
-                               value="<?php echo $_SESSION['MARRIAGE']['HUSBAND']['husband_surname'] ?>" minlength="2"
+                        <label for="customer_surname" class="visually-hidden">Фамилия</label>
+                        <input type="text" class="form-control" id="customer_surname" name="customer_surname"
+                               placeholder="Фамилия"
+                               value="<?php echo $_SESSION['DEATHH']['customer_surname'] ?>" minlength="2"
                                maxlength="33"
                                required="">
                     </div>
                     <div class="col-12">
-                        <label for="husband_name" class="visually-hidden">Имя мужа</label>
-                        <input type="text" class="form-control" id="husband_name" name="husband_name"
-                               placeholder="Имя мужа"
-                               value="<?php echo $_SESSION['MARRIAGE']['HUSBAND']['husband_name'] ?>" minlength="2"
+                        <label for="customer_name" class="visually-hidden">Имя</label>
+                        <input type="text" class="form-control" id="customer_name" name="customer_name"
+                               placeholder="Имя"
+                               value="<?php echo $_SESSION['DEATHH']['customer_name'] ?>" minlength="2"
                                maxlength="33"
                                required="">
                     </div>
                     <div class="col-12">
-                        <label for="husband_middleName" class="visually-hidden">Отчество мужа</label>
-                        <input type="text" class="form-control" id="husband_middleName" name="husband_middleName"
-                               placeholder="Отчество мужа"
-                               value="<?php echo $_SESSION['MARRIAGE']['HUSBAND']['husband_middleName'] ?>"
+                        <label for="customer_middleName" class="visually-hidden">Отчество</label>
+                        <input type="text" class="form-control" id="customer_middle_name" name="customer_middle_name"
+                               placeholder="Отчество"
+                               value="<?php echo $_SESSION['DEATHH']['customer_middle_name'] ?>"
                                minlength="2"
                                maxlength="33"
                                required="">
                     </div>
-                    <div class="col-12 <? if ($_SESSION['MARRIAGE']['HUSBAND']['visually_hidden'] == false) {
-                        echo 'visually-hidden';
-                    } else {
-                        echo '';
-                    } ?>">
-                        <?
-                        //вывод ошибок
-                        if ($_SESSION['MARRIAGE']['reg'] == false & !empty($_SESSION['MARRIAGE']['HUSBAND']['ERRORS']))
-                        {
-                            foreach ($_SESSION['MARRIAGE']['HUSBAND']['ERRORS'] as $error)
-                            {
-                                echo '<div class="alert alert-danger mb-1 p-1 text-center" role="alert">' . $error . '</div>';
-                            }
-                        }
-                        ?>
-                    </div><!--ошибки-->
-                    <!--02 - Муж - Close -->
+                    <div class="col-auto">
+                        <label for="date_death" class="">Дата наступления смерти</label>
+                        <input type="date" class="form-control" id="date_death" name="date_death"
+                               placeholder="Дата расторжения брака 0000 00 00"
+                               value="<?php echo $_SESSION['DEATHH']['date_death'] ?>" required="">
+                    </div>
+                    <!--02 - Customer - Close -->
 
                     <div class="col-12 my-1">
                         <?
                         //вывод ошибок
-                        if ($_SESSION['MARRIAGE']['reg'] == false & !empty($_SESSION['MARRIAGE']['ERRORS']))
+                        if (!empty($_SESSION['DEATHH']['ERRORS']))
                         {
-                            foreach ($_SESSION['MARRIAGE']['ERRORS'] as $error)
+                            foreach ($_SESSION['DEATHH']['ERRORS'] as $error)
                             {
                                 echo '<div class="alert alert-danger mb-1 p-1 text-center" role="alert">' . $error . '</div>';
                             }
+                            $_SESSION['DEATHH']['ERRORS'] = [];
                         }
 
                         //вывод успешный действий
-                        if ($_SESSION['MARRIAGE']['reg'] == true & !empty($_SESSION['MARRIAGE']['SUCCESS']))
+                        if (!empty($_SESSION['DEATHH']['SUCCESS']))
                         {
                             {
-                                foreach ($_SESSION['MARRIAGE']['SUCCESS'] as $success)
+                                foreach ($_SESSION['DEATHH']['SUCCESS'] as $success)
                                 {
                                     echo '<div class="alert alert alert-success mb-1 p-1 text-center" role="alert">' . $success . '</div>';
                                 }
                             }
-
-                            $_SESSION['MARRIAGE']['reg'] = false;
+                            $_SESSION['DEATHH']['SUCCESS'] = [];
                         }
                         ?>
                     </div>
+
                 </div>
-                <button class="w-100 btn btn-primary btn-lg" type="submit">Создать</button>
+                <button class="w-100 btn btn-primary btn-lg" type="submit">Регистрация</button>
             </form>
         </div>
     </div>
--- controllers/actsDeathController.php
+++ controllers/actsDeathController.php
@@ -14,4 +14,147 @@ if (!R::testConnection())
 }
 /*02-Подключение к базе данных*/
 
-myDump($_POST);
+//myDump($_POST);
+
+death_act_register();
+
+function death_act_register()
+{
+    fill_session($_POST);
+    data_validate();
+
+    $thereIsDeathAct = true;
+
+    $customer_id = check_customer();
+    if ($customer_id != NULL)
+    {
+        $thereIsDeathAct = check_death_act($customer_id);
+    }
+
+    if ($thereIsDeathAct == false)
+    {
+        save_death_act($customer_id, $_SESSION['staff_id']);
+    }
+
+    clear_session();
+
+    header('location: ../acts_death_create.php');
+}
+
+function fill_session($post)
+{
+    $_SESSION['DEATHH']['customer_surname'] = $post['customer_surname'];
+    $_SESSION['DEATHH']['customer_name'] = $post['customer_name'];
+    $_SESSION['DEATHH']['customer_middle_name'] = $post['customer_middle_name'];
+    $_SESSION['DEATHH']['date_death'] = $post['date_death'];
+
+    $_SESSION['DEATHH']['SUCCESS'] = [];
+    $_SESSION['DEATHH']['ERRORS'] = [];
+}
+
+function data_validate()
+{
+    //surname
+    if (preg_match('/[^,\p{Cyrillic}]/ui', $_SESSION['DEATHH']['customer_surname']))
+    {
+        //если не проходит проверку
+        array_push($_SESSION['DEATHH']['ERRORS'], "Фамилия может содержать только кириллицу");
+    }
+
+    //name
+    if (preg_match('/[^,\p{Cyrillic}]/ui', $_SESSION['DEATHH']['customer_name']))
+    {
+        //если не проходит проверку
+        array_push($_SESSION['DEATHH']['ERRORS'], "Имя может содержать только кириллицу");
+    }
+
+    //middleName
+    if (preg_match('/[^,\p{Cyrillic}]/ui', $_SESSION['DEATHH']['customer_middle_name']))
+    {
+        //если содержит не только кириллицу
+        array_push($_SESSION['DEATHH']['ERRORS'], "Отчество может содержать только кириллицу");
+    }
+
+    /* ДОПУСТИМ ТУТ БУДЕТ ВАЛИДАЦИЯ ДАТЫ  */
+}
+
+function check_customer()
+{
+    //Поиск нужного пользователя
+    $customer_id = R::findOne('customers',
+        'surname = ? AND
+             name = ? AND
+             middle_name = ?', [
+            $_SESSION['DEATHH']['customer_surname'],
+            $_SESSION['DEATHH']['customer_name'],
+            $_SESSION['DEATHH']['customer_middle_name']]);
+
+    if ($customer_id != NULL)
+    {
+        //если есть такой клиент, говорим что такой уже есть
+        return $customer_id;
+
+    } else
+    {
+        array_push($_SESSION['DEATHH']['ERRORS'], "Клиент не зарегистрирован. Сначала зарегистрируйте его");
+        return NULL;
+    }
+}
+
+function check_death_act($customer_id)
+{
+    $customer = R::load('customers', $customer_id);
+    $customerDeathActs = $customer->alias('customer')->ownDeathactsList;
+    if (!empty($customerDeathActs))
+    {
+        array_push($_SESSION['DEATHH']['ERRORS'], "У данного гражданина уже зарегистрировано свидетельство о смерти");
+        return true;
+    } else
+    {
+        return false;
+    }
+}
+
+function save_death_act($customer_id, $staff_id)
+{
+    $customer = R::load('customers', $customer_id);
+
+    $staff = R::load('staff', $staff_id);
+
+    $actType = R::load('actstypes', 4);//свидетельство о смерти
+
+    $deathActs = R::dispense('deathacts');
+    $deathActs->date_death = $_SESSION['DEATHH']['date_death'];
+    $deathActs->customer = $customer;
+
+    $deathActs->staff = $staff;
+
+    //создаю таблицу пользотели и книги
+    $usersAndBookActs = R::dispense('usersandbookacts');
+    $usersAndBookActs->locality = NULL;
+    $usersAndBookActs->year = date('Y');//дата внесения в базу?
+    $usersAndBookActs->act_types = $actType;
+
+    $usersAndBookActs->divorce_acts = NULL;
+    $usersAndBookActs->marriage_acts = NULL;
+    $usersAndBookActs->birth_acts = NULL;
+    $usersAndBookActs->death_acts = $deathActs;
+    $usersAndBookActs->adoption_acts = NULL;
+
+    //сохраняю marriageacts
+    R::store($deathActs);
+
+    //сохраняю usersandbookacts
+    R::store($usersAndBookActs);
+
+    array_push($_SESSION['DEATHH']['SUCCESS'], "Свидетельство о смерти успешно зарегистрировано");
+}
+
+function clear_session()
+{
+    $_SESSION['DEATHH']['customer_surname'] = '';
+    $_SESSION['DEATHH']['customer_name'] = '';
+    $_SESSION['DEATHH']['customer_middle_name'] = '';
+    $_SESSION['DEATHH']['date_death'] = NULL;
+}
+
