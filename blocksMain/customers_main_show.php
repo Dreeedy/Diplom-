108828054e4c091b4daeeb9e5a9bd5f454aa9fcc
Dreeedy <dreeedyone@gmail.com>
2021-03-24 15:54:43 +0300

    adoption acts / customers_main_show.php / коммент "выбран"

--- blocksMain/customers_main_show.php
+++ blocksMain/customers_main_show.php
@@ -36,13 +36,13 @@
 <!--01 Кусок таблицы-->
     <div class="pt-2 pb-1 row">
         <h2 class="col-auto">Клиенты</h2>
-        <div class="col btn-outline-primary btn text-start my-auto">
-            Выбран: <? echo $_GET['customer_surname'].' '.
+        <!--<div class="col btn-outline-primary btn text-start my-auto">
+            Выбран: <?/* echo $_GET['customer_surname'].' '.
                 $_GET['customer_name'].' '.
                 $_GET['customer_middle_name'].' '.
                 $_GET['customer_phone_number'].' '.
-                $_GET['customer_address'];?>
-        </div>
+                $_GET['customer_address'];*/?>
+        </div>-->
     </div>
 
     <div class="overflow-auto" style="max-height: 663px">
