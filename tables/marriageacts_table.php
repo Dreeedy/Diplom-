b42b04521500626ade6abc1e280454cf64e4dec9
Dreeedy <dreeedyone@gmail.com>
2021-03-26 20:30:03 +0300

    create marriageacts_table.php

--- tables/marriageacts_table.php
+++ tables/marriageacts_table.php
@@ -4,37 +4,28 @@ $marriageacts_arr = R::findall('marriageacts');
 
 $unicle_data = $_SESSION['SHOW']['unicle_data'];
 
-function hui($usersandbookacts_arr, $unicle_data)
+$marriageacts_arr_local = [];
+foreach ($usersandbookacts_arr as $uba)
 {
-    $marriageacts_arr_local = [];
-    foreach ($usersandbookacts_arr as $uba)
+    if ($uba->year == $unicle_data & $uba->act_types_id == "1")// 1 - id - свидетельство о заключении брака
     {
-        if ($uba->year == $unicle_data & $uba->act_types_id == "1")// 1 - id - свидетельство о заключении брака
-        {
-            myDump($uba->marriage_acts);
-            $marriageacts_arr_local[] = $uba->marriageacts;
-        }
+        $marriageacts_arr_local[] = $uba->marriageacts;
     }
-    return $marriageacts_arr_local;
 }
-
-hui($usersandbookacts_arr, $unicle_data);
-
-
 ?>
 <table class="table table-striped table-hover">
     <thead>
     <tr>
         <th scope="col">#</th>
-        <th scope="col">Муж</th>
-        <th scope="col">Жена</th>
+        <th scope="col">ФИО Мужа</th>
+        <th scope="col">ФИО Жены</th>
         <th scope="col">Дата заключения</th>
     </tr>
     </thead>
     <tbody>
     <?
     $i = 1;
-    foreach ($marriageacts_arr as $marr_act)
+    foreach ($marriageacts_arr_local as $marr_act)
     {
         $husband = $marr_act->fetchAs('customers')->husband;
         $husbandFIO = $husband->surname.' '.$husband->name.' '.$husband->middle_name;
