172700f1b1e4485ccb540e85c82aa2c2a3b637f9
Dreeedy <dreeedyone@gmail.com>
2021-03-28 15:50:02 +0300

    Горизонтальная и вертикальная менюшка

---
+++ blocksMain/horizontal_menu/horizontalMenu.php
@@ -0,0 +1,32 @@
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-1 pt-5">
+    <div class="col">
+        <a href="../../customers_register.php">
+            <button class="w-100 btn btn-md btn-primary text-start">Регистрация клиента</button>
+        </a>
+    </div>
+    <div class="col">
+        <a href="../../acts_marriage_create.php">
+            <button class="w-100 btn btn-md btn-primary text-start">Регистрация акта о бракосочетании</button>
+        </a>
+    </div>
+    <div class="col">
+        <a href="../../index.php">
+            <button class="w-100 btn btn-md btn-outline-danger text-start">На главную</button>
+        </a>
+    </div>
+    <div class="col">
+        <a href="../../acts_birth_create.php">
+            <button class="w-100 btn btn-md btn-primary text-start">Регистрация акта о рождении ребенка</button>
+        </a>
+    </div>
+    <div class="col">
+        <a href="../../acts_adoption_create.php">
+            <button class="w-100 btn btn-md btn-primary text-start">Регистрация акта о усыновлении</button>
+        </a>
+    </div>
+    <div class="col">
+        <a href="../../acts_death_create.php">
+            <button class="w-100 btn btn-md btn-primary text-start">регистрация акта о смерти</button>
+        </a>
+    </div>
+</div>
---
+++ blocksMain/verical_menu/verticalMenu.php
@@ -0,0 +1,35 @@
+<h4 class="d-flex justify-content-between align-items-center mb-3">
+    <span class="text-muted">Меню</span>
+</h4>
+<div class="list-group mb-3">
+    <div class="mb-1">
+        <a href="../../index.php">
+            <button class="w-100 btn btn-md btn-outline-danger text-start">На главную</button>
+        </a>
+    </div>
+    <div class="mb-1">
+        <a href="../../customers_register.php">
+            <button class="w-100 btn btn-md btn-primary text-start">Регистрация клиента</button>
+        </a>
+    </div>
+    <div class="mb-1">
+        <a href="../../acts_marriage_create.php">
+            <button class="w-100 btn btn-md btn-primary text-start">Регистрация акта о бракосочетании</button>
+        </a>
+    </div>
+    <div class="mb-1">
+        <a href="../../acts_birth_create.php">
+            <button class="w-100 btn btn-md btn-primary text-start">Регистрация акта о рождении ребенка</button>
+        </a>
+    </div>
+    <div class="mb-1">
+        <a href="../../acts_adoption_create.php">
+            <button class="w-100 btn btn-md btn-primary text-start">Регистрация акта о усыновлении</button>
+        </a>
+    </div>
+    <div class="mb-1">
+        <a href="../../acts_death_create.php">
+            <button class="w-100 btn btn-md btn-primary text-start">регистрация акта о смерти</button>
+        </a>
+    </div>
+</div>
