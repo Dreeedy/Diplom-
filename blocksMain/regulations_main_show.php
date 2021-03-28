16fa5fa1321df351a7ff53503263d1f3c7b2e914
Dreeedy <dreeedyone@gmail.com>
2021-03-28 15:50:22 +0300

    Всякая доп инфа о организации

---
+++ blocksMain/regulations_main_show.php
@@ -0,0 +1,32 @@
+<div class="container" style="min-height: 873px;">
+    <!--01 Горизонтальное меню-->
+    <? require_once 'horizontal_menu/horizontalMenu.php' ?>
+    <!--02 Горизонтальное меню-->
+    <!--01 Кусок таблицы-->
+    <div class="pt-2 pb-1 row">
+        <h2 class="col-auto">Нормативные документы</h2>
+        <!--<div class="col btn-outline-primary btn text-start my-auto">
+            Выбран: <? /* echo $_GET['customer_surname'].' '.
+                $_GET['customer_name'].' '.
+                $_GET['customer_middle_name'].' '.
+                $_GET['customer_phone_number'].' '.
+                $_GET['customer_address'];*/ ?>
+        </div>-->
+    </div>
+
+    <div class="overflow-auto" style="max-height: 663px">
+        <hr>
+
+        <div>
+            <ul>
+                <li class="pb-2">Конституция Республики Татарстан</li>
+                <li class="pb-2">Международное законодательство</li>
+                <li class="pb-2">Федеральное законодательство</li>
+                <li class="pb-2">Законодательство Республики Татарстан</li>
+                <li class="pb-2">Международная правовая помощь и истребование документов</li>
+                <li class="pb-2">Нормативные документы Управления ЗАГС Кабинета Министров Республики Татарстан</li>
+            </ul>
+        </div>
+    </div>
+    <!--02 Кусок таблицы-->
+</div>
---
+++ blocksMain/where_are_we_main_show.php
@@ -0,0 +1,52 @@
+<div class="container" style="min-height: 873px;">
+    <!--01 Горизонтальное меню-->
+    <? require_once 'horizontal_menu/horizontalMenu.php'?>
+    <!--02 Горизонтальное меню-->
+    <!--01 Кусок таблицы-->
+    <div class="pt-2 pb-1 row">
+        <h2 class="col-auto">Отдел ЗАГС ИК МО ЛМР РТ</h2>
+        <!--<div class="col btn-outline-primary btn text-start my-auto">
+            Выбран: <?/* echo $_GET['customer_surname'].' '.
+                $_GET['customer_name'].' '.
+                $_GET['customer_middle_name'].' '.
+                $_GET['customer_phone_number'].' '.
+                $_GET['customer_address'];*/?>
+        </div>-->
+    </div>
+
+    <div class="" style="max-height: 663px">
+        <p>
+            На данной странице указаны контактные данные компании Отдел ЗАГС ИК МО ЛМР РТ, расположенной по адресу Лениногорск, улица Булгакова, 1. С представителями этой организации можно связаться по телефону +78559565646, +78559565647, и уточнить у них интересующие вас вопросы.
+        </p>
+
+        <hr>
+
+        <div>
+            <div class="row col-12">
+                <div class="col-auto" style="min-width: 120px">
+                    <b>Адрес:</b>
+                </div>
+                <div class="col-auto">
+                    <p>Лениногорск, улица Булгакова, 1</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-auto" style="min-width: 120px">
+                    <b>График:</b>
+                </div>
+                <div class="col-auto">
+                    <p>вт,ср 8:00-17:00, перерыв 12:00-13:00, чт,сб 8:00-12:00</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-auto" style="min-width: 120px">
+                    <b>Телефон:</b>
+                </div>
+                <div class="col-auto">
+                    <p>+7 855 956-56-46    +7 855 956-56-47</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--02 Кусок таблицы-->
+</div>
