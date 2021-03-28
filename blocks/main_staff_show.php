f04ba3c40d3305dbc6132847f7cd6590e5228ad0
Dreeedy <dreeedyone@gmail.com>
2021-03-28 16:11:37 +0300

    Переделал вывод админки

--- blocks/main_staff_show.php
+++ blocks/main_staff_show.php
@@ -1,19 +1,21 @@
-<div class="container">
-    <div style="min-height: 873px;">
-
-        <div class="row flex-nowrap justify-content-between align-items-center">
-            <div class="col-4 pt-1">
-                <a class="btn btn-sm btn-outline-danger" href="../admin.php">Назад</a>
-            </div>
-            <div class="col-4 text-center">
-                <h2 class="text-center">Персонал</h2>
-            </div>
-            <div class="col-4 d-flex justify-content-end align-items-center">
-
-            </div>
-        </div>
-        <div class="">
-            <?php require "blocks/main_staff_show_table.php" ?>
-        </div>
+<div class="container" style="min-height: 873px;">
+    <!--01 Горизонтальное меню-->
+    <? require_once './blocksMain/horizontal_menu/horizontalMenu.php'?>
+    <!--02 Горизонтальное меню-->
+    <!--01 Кусок таблицы-->
+    <div class="pt-2 pb-1 row">
+        <h2 class="col-auto">Персонал</h2>
+        <!--<div class="col btn-outline-primary btn text-start my-auto">
+            Выбран: <?/* echo $_GET['customer_surname'].' '.
+                $_GET['customer_name'].' '.
+                $_GET['customer_middle_name'].' '.
+                $_GET['customer_phone_number'].' '.
+                $_GET['customer_address'];*/?>
+        </div>-->
     </div>
+
+    <div class="overflow-auto" style="max-height: 663px">
+        <? require_once "main_staff_show_table.php" ?>
+    </div>
+    <!--02 Кусок таблицы-->
 </div>
