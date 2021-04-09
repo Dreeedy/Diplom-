fe8ec260f3f1a55b36e9e9921e9b99a7c871b004
Dreeedy <dreeedyone@gmail.com>
2021-04-09 14:34:54 +0300

    create statistic_menu.php

---
+++ blocks/statistic_menu.php
@@ -0,0 +1,16 @@
+<div class="col">
+    <div class="card shadow-sm">
+
+        <img class="my-1 mx-auto img-fluid px-2" src="../img/statistic_img.svg" alt="" width="256" height="256">
+        <h3 class="mx-auto px-2">Статистика</h3>
+        <div class="card-body px-2">
+            <a href="../acts_adoption_create.php" class="text-decoration-none">
+                <button type="button" class="btn btn-sm btn-outline-secondary">Статистика</button>
+            </a>
+            <a href="../acts_adoption_create.php" class="text-decoration-none">
+                <button type="button" class="btn btn-sm btn-outline-secondary">Отчеты</button>
+            </a>
+        </div>
+
+    </div>
+</div>
